3649e25161cc81b70c3cd016dee8ee0f1d91ad5d
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-06-07 16:04:23 +0545

    created controller and model for user

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import 'dotenv/config';
+import userRouter from './Routes/userRouter.js';
 
 const app = express();
 const port = process.env.PORT || 8081;
@@ -18,6 +19,8 @@ const port = process.env.PORT || 8081;
 
   // Routes
   app.get('/', (req, res) => res.send("API is working"));
+  app.use('/api/user',userRouter)
+  
 
   // Start server
   app.listen(port, () => {
