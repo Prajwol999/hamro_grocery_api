9c72578db231b8b31e125482b271c00752ea5b7d
Prajwol999 <130135344+Prajwol999@users.noreply.github.com>
2025-06-22 21:57:23 +0545

    added controller for order

--- server/Routes/orderRoutes.js
+++ server/Routes/orderRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getOrders, updateOrderStatus } from '../controllers/orderController.js';
+
+import { getOrders, updateOrderStatus, createOrder, getMyOrders, getOrderById } from '../controllers/orderController.js';
 import { authenticateUser, isAdmin } from '../middleware/authorizedUser.js';
 
 const router = express.Router();
 
+router.post('/', authenticateUser, createOrder);
+router.get('/myorders', authenticateUser, getMyOrders);
 router.get('/', authenticateUser, isAdmin, getOrders);
+router.get('/:id', authenticateUser, isAdmin, getOrderById);
 router.put('/:id', authenticateUser, isAdmin, updateOrderStatus);
 
 export default router;
